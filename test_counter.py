29355ca2e84304970aefe4f19dac54635ec8aa4b
Brook Kassa <brookassa4@gmail.com>
2025-05-21 13:40:38 -0400

    fixed structure some more

--- test_counter.py
+++ test_counter.py
@@ -1,10 +1,11 @@
 import sys
 import os
 
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'VisitorCounter'))
 
-import __init__ as function_code  
+import visitor  
 
 for _ in range(3):
-    response = function_code.main(None)
-    print(response.get_body().decode())
+    result = visitor.main(None)
+    print(result.get_body().decode())
